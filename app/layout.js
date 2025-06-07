53c55833363670680b539ff6fe55f98e89b53d0e
lokesh <lokesh24mittal@gmail.com>
2025-06-07 16:00:03 +0530

    authentication clerk

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const inter=Inter({subsets:["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Splitr",
@@ -16,14 +17,13 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/logos/logo-s.png" sizes="any" />
       </head>
-      <body
-        className={`${inter.className}`}
-      >
-       
+      <body className={`${inter.className}`}>
         <main className="min-h-screen">
- <ConvexClientProvider>
-        <Header/>
-        </ConvexClientProvider>
+          <ClerkProvider>
+            <ConvexClientProvider>
+              <Header />
+            </ConvexClientProvider>
+          </ClerkProvider>
           {children}
         </main>
       </body>

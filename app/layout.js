a1dbe22d5e2066748c0b6c470915e9878fe2a1b1
lokesh <lokesh24mittal@gmail.com>
2025-06-07 15:47:09 +0530

    configure-backend-convex

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
 
 const inter=Inter({subsets:["latin"]});
 
@@ -18,9 +19,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.className}`}
       >
-        <Header/>
+       
         <main className="min-h-screen">
-
+ <ConvexClientProvider>
+        <Header/>
+        </ConvexClientProvider>
           {children}
         </main>
       </body>

eb07a40d3dd7ee726e190075bba81a9d9d826601
lokesh <lokesh24mittal@gmail.com>
2025-06-07 15:31:23 +0530

    initialize and app layout

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+
+const inter=Inter({subsets:["latin"]});
+
+export const metadata = {
+  title: "Splitr",
+  description: "The smart way to split bills with friends",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logos/logo-s.png" sizes="any" />
+      </head>
+      <body
+        className={`${inter.className}`}
+      >
+        <Header/>
+        <main className="min-h-screen">
+
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
